ff47b7681c9f6bcf839f0b83e3348081549f2148
Soki <soki.fin02@gmail.com>
2024-09-13 21:05:00 +0300

    move shunting yard function to separate module

---
+++ src/shunting_yard.py
@@ -0,0 +1,32 @@
+import re
+
+def is_number(token):
+    return re.match(r"^-?\d+(\.\d+)?$", token)
+
+def shunting_yard_algorithm(equation):
+    tokens = re.findall(r'\d+|[+*/^()-]', equation)
+    
+    operators_precedence = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3}
+    
+    holding_stack = []
+    output = []
+    
+    for token in tokens:
+        if is_number(token):
+            output.append(token)
+        elif token in operators_precedence:
+            while (holding_stack and holding_stack[-1] in operators_precedence and
+                   operators_precedence[holding_stack[-1]] >= operators_precedence[token]):
+                output.append(holding_stack.pop())
+            holding_stack.append(token)
+        elif token == "(":
+            holding_stack.append(token)
+        elif token == ")":
+            while holding_stack and holding_stack[-1] != "(":
+                output.append(holding_stack.pop())
+            holding_stack.pop() 
+
+    while holding_stack:
+        output.append(holding_stack.pop())
+
+    return output
