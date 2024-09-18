379d16d0b84d7087af8ad73dfeaa3d73a246162e
Soki <soki.fin02@gmail.com>
2024-09-18 21:33:16 +0300

    add loop for equation input until exit

--- src/index.py
+++ src/index.py
@@ -1,11 +1,19 @@
 from shunting_yard import shunting_yard_algorithm
 from evaluate_postfix import evaluate_postfix
 
-equation = input("Kirjoita yhtälö: ")
-output = shunting_yard_algorithm(equation)
+while True:
+    equation = input("Kirjoita yhtälö ('exit' lopettaa): ")
+    
+    if equation.lower() == "exit":
+        print("Ohjelma lopetetaan.")
+        break
+    
+    try:
+        output = shunting_yard_algorithm(equation)
+        print(f"Postfix-muoto: {output}")
 
-print(f"output: {output}")
+        result = evaluate_postfix(output)
+        print(f"Tulos: {result}")
+    except Exception:
+        print("syötteessä virhe")
 
-result = evaluate_postfix(output)
-
-print(f"result: {result}")
