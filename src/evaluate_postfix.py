ec66cd2516320d591a374930b6d8bba9c6e1abb3
Soki <soki.fin02@gmail.com>
2024-09-18 21:35:39 +0300

    add support for sqrt

--- src/evaluate_postfix.py
+++ src/evaluate_postfix.py
@@ -1,9 +1,14 @@
+import math
+
 def evaluate_postfix(expression):
     stack = []
 
     for token in expression:
         if token.isdigit():
             stack.append(int(token))
+        elif token == 'sqrt':
+            val = stack.pop()
+            stack.append(math.sqrt(val))
         else:
             val2 = stack.pop()
             val1 = stack.pop()
--- src/shunting_yard.py
+++ src/shunting_yard.py
@@ -4,8 +4,8 @@ def is_number(token):
     return re.match(r"^-?\d+(\.\d+)?$", token)
 
 def shunting_yard_algorithm(equation):
-    tokens = re.findall(r'\d+|[+*/^()-]', equation)
-    
+    tokens = re.findall(r'\d+|[+*/^()-]|sqrt', equation)
+
     operators_precedence = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3}
     
     holding_stack = []
@@ -14,6 +14,8 @@ def shunting_yard_algorithm(equation):
     for token in tokens:
         if is_number(token):
             output.append(token)
+        elif token == "sqrt":
+            holding_stack.append(token)
         elif token in operators_precedence:
             while (holding_stack and holding_stack[-1] in operators_precedence and
                    operators_precedence[holding_stack[-1]] >= operators_precedence[token]):
@@ -24,7 +26,9 @@ def shunting_yard_algorithm(equation):
         elif token == ")":
             while holding_stack and holding_stack[-1] != "(":
                 output.append(holding_stack.pop())
-            holding_stack.pop() 
+            holding_stack.pop()
+            if holding_stack and holding_stack[-1] == "sqrt":
+                output.append(holding_stack.pop())
 
     while holding_stack:
         output.append(holding_stack.pop())
