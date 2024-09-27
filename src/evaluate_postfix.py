13e11060b327a30ce65da477f20b7432ab2219d5
Soki <soki.fin02@gmail.com>
2024-09-28 00:31:17 +0300

    add error handling

--- src/evaluate_postfix.py
+++ src/evaluate_postfix.py
@@ -7,18 +7,28 @@ def evaluate_postfix(expression):
         if token.isdigit():
             stack.append(int(token))
         elif token == 'sqrt':
+            if not stack:
+                raise ValueError("Virhe: Yritettiin poistaa arvo tyhjältä pinolta.")
             val = stack.pop()
             stack.append(math.sqrt(val))
         elif token == 'sin':
+            if not stack:
+                raise ValueError("Virhe: Yritettiin poistaa arvo tyhjältä pinolta.")
             val = stack.pop()
             stack.append(math.sin(val))
         elif token == 'cos':
+            if not stack:
+                raise ValueError("Virhe: Yritettiin poistaa arvo tyhjältä pinolta.")
             val = stack.pop()
             stack.append(math.cos(val))
         elif token == 'tan':
+            if not stack:
+                raise ValueError("Virhe: Yritettiin poistaa arvo tyhjältä pinolta.")
             val = stack.pop()
             stack.append(math.tan(val)) 
         else:
+            if len(stack) < 2:
+                raise ValueError("Virhe: Yritettiin poistaa arvoja tyhjältä pinolta.")
             val2 = stack.pop()
             val1 = stack.pop()
 
@@ -32,5 +42,8 @@ def evaluate_postfix(expression):
                 stack.append(val1 / val2)
             elif token == '^':
                 stack.append(val1 ** val2)
+
+    if len(stack) != 1:
+        raise ValueError("Virhe: Tyhjentämättömiä arvoja pinossa.")
 
     return stack[0]
--- src/index.py
+++ src/index.py
@@ -1,20 +1,20 @@
 from shunting_yard import shunting_yard_algorithm
 from evaluate_postfix import evaluate_postfix
 
-while True: # pragma: no cover
+while True:  # pragma: no cover
     equation = input("Kirjoita yhtälö ('exit' lopettaa): ")
-    
+
     if equation.lower() == "exit":
         print("Ohjelma lopetetaan.")
         break
-    
+
     try:
         output = shunting_yard_algorithm(equation)
         print(f"Postfix-muoto: {output}")
 
         result = evaluate_postfix(output)
         print(f"Tulos: {result}")
-    except Exception:
-        print("syötteessä virhe")
-
-
+    except ValueError as ve:
+        print(f"Syötteessä virhe: {ve}")
+    except Exception as e:
+        print(f"Virhe: {e}")
