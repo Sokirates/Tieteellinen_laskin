87ee9ad503cb61fc044ca0d88f1eef8d8e03504f
Soki <soki.fin02@gmail.com>
2024-10-15 14:54:25 +0300

    update valueerror messages

--- src/variable_manager.py
+++ src/variable_manager.py
@@ -2,12 +2,17 @@ variables = {}
 
 def define_variable(name, value):
     """
-    Define a variable with a given name and value.
+    Määritä muuttuja annetulla nimellä ja arvolla.
     """
     if len(name) != 1 or not name.isalpha():
         raise ValueError("Muuttujan nimen tulee olla vain yksi kirjain.")
+    
+    if not isinstance(value, (int, float)):
+        raise ValueError("Anna kelvollinen numero muuttujan arvoksi.")
+    
     variables[name] = value
     print(f"Muuttuja '{name}' määritelty arvolla {value}.")
+
 
 def get_variable(name):
     """
